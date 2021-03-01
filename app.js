6df2dd2e418bdacf209a9208b8379c1e7b188f6d
Mourad <mourad777b@gmail.com>
2021-03-01 11:23:23 -0600

    update mongo records

--- app.js
+++ app.js
@@ -205,8 +205,8 @@ mongoose
   .then(async (result) => {
     console.log("connected to mongoose");
 
-    await Instructor.update({},{isAccountApproved:true,isAccountSuspended:false});
-    await Student.update({},{isAccountApproved:true,isAccountSuspended:false});
+    await Instructor.update({},{isAccountApproved:true,isAccountSuspended:false},{multi:true});
+    await Student.update({},{isAccountApproved:true,isAccountSuspended:false},{multi:true});
 
     const expressServer = app.listen(port);
     const io = require("./socket").init(expressServer);
