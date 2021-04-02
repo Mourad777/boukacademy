a3e25e9ebb0d1a9627bab0fd3367c7397811f25a
Mourad <mourad777b@gmail.com>
2021-04-02 17:01:41 -0600

    stripe key fix

--- middleware/auth.js
+++ middleware/auth.js
@@ -6,21 +6,25 @@ const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID)
 
 module.exports = async (req, res, next) => {
   const authHeader = req.get('Authorization');
-  console.log('authHeader',authHeader)
+  console.log('authHeader', authHeader)
   if (!authHeader) {
     console.log('noheader')
     req.studentIsAuth = false;
     req.instructorIsAuth = false;
     console.log('next')
     next();
-    
+
   }
 
 
 
 
 
-  const token = authHeader.split(' ')[1];
+  let token;
+  if (authHeader) {
+    token = authHeader.split(' ')[1];
+  }
+
   let userType, id, errorMessage;
   if (!token || token === "undefined") {
     console.log('no token')
@@ -61,7 +65,7 @@ module.exports = async (req, res, next) => {
         id = decodedToken.studentId
       }
       const user = await require(`../models/${userType}`).findById(id)
-      if(!user){
+      if (!user) {
         next()
       }
       const isAccountSuspended = user.isAccountSuspended
@@ -116,15 +120,15 @@ module.exports = async (req, res, next) => {
   } else {
 
     let ticket;
-    console.log('process.env.GOOGLE_CLIENT_ID',process.env.GOOGLE_CLIENT_ID);
-    console.log('token',token)
+    console.log('process.env.GOOGLE_CLIENT_ID', process.env.GOOGLE_CLIENT_ID);
+    console.log('token', token)
     try {
       console.log('trying to get ticket')
       ticket = await client.verifyIdToken({
         idToken: token,
         audience: process.env.GOOGLE_CLIENT_ID,
       });
-      console.log('ticket...: ',ticket)
+      console.log('ticket...: ', ticket)
     } catch (e) {
       console.log('failed to get google ticket')
     }
@@ -133,10 +137,10 @@ module.exports = async (req, res, next) => {
         const payload = ticket.getPayload()
         req.googleUser = payload
         req.isGoogleAuth = true
-        console.log('payload: ',payload)
+        console.log('payload: ', payload)
         console.log('next')
         next();
-        
+
       } catch (e) {
         console.log('e', e)
       }
@@ -144,11 +148,11 @@ module.exports = async (req, res, next) => {
     // else {
     //   return next()
     // }
-    console.log('isticket',!!ticket)
+    console.log('isticket', !!ticket)
     console.log('no token nor ticket')
     // console.log('next')
     // next();
-    
+
 
   }
 };
