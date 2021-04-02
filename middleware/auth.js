c02b5a4760c6032a786be396d60abb22ef45d2f2
Mourad <mourad777b@gmail.com>
2021-04-02 17:57:53 -0600

    web hooks coinbase

--- middleware/auth.js
+++ middleware/auth.js
@@ -6,149 +6,156 @@ const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID)
 
 module.exports = async (req, res, next) => {
   const authHeader = req.get('Authorization');
-  console.log('authHeader',authHeader)
+  console.log('authHeader', authHeader)
   if (!authHeader) {
     console.log('noheader')
     req.studentIsAuth = false;
     req.instructorIsAuth = false;
     console.log('next')
     next();
-    
-  }
-
-
-
-
-
-  const token = authHeader.split(' ')[1];
-  let userType, id, errorMessage;
-  if (!token || token === "undefined") {
-    console.log('no token')
-    req.studentIsAuth = false;
-    req.instructorIsAuth = false;
-    console.log('no token so next')
-    next();
-  }
-
-
-  let decodedToken;
-  try {
-    decodedToken = jwt.verify(token, process.env.SECRET);
-  } catch (err) {
-    // console.log('err', err)
-    // return res.status(403).send(error);
 
   }
 
 
-  if (decodedToken) {
-    if (!decodedToken) {
+
+
+
+
+  if (authHeader) {
+
+
+
+
+    const token = authHeader.split(' ')[1];
+    let userType, id, errorMessage;
+    if (!token || token === "undefined") {
+      console.log('no token')
       req.studentIsAuth = false;
       req.instructorIsAuth = false;
+      console.log('no token so next')
+      next();
     }
 
-    if (decodedToken.studentId) {
-      userType = 'student'
-    }
-    if (decodedToken.instructorId) {
-      userType = 'instructor'
-    }
+
+    let decodedToken;
     try {
-      if (userType === 'instructor') {
-        id = decodedToken.instructorId
-      }
-      if (userType === 'student') {
-        id = decodedToken.studentId
-      }
-      const user = await require(`../models/${userType}`).findById(id)
-      if(!user){
-        next()
-      }
-      const isAccountSuspended = user.isAccountSuspended
-      const isAccountApproved = user.isAccountApproved
+      decodedToken = jwt.verify(token, process.env.SECRET);
+    } catch (err) {
+      // console.log('err', err)
+      // return res.status(403).send(error);
 
-      const admin = await Instructor.findOne({ admin: true }).populate(
-        "configuration"
-      );
-      const adminSettings = admin._doc.configuration;
-      const isApproveStudentAccounts = adminSettings.isApproveStudentAccounts;
-      const isApproveInstructorAccounts = adminSettings.isApproveInstructorAccounts;
-      if (isAccountSuspended) {
-        errorMessage = 'accountSuspended'
-        const error = new Error("accountSuspended");
-        error.code = 403;
-        throw error;
-      } else if (!isAccountApproved && userType === 'student' && isApproveStudentAccounts) {
-        errorMessage = 'accountNotActivated'
-        const error = new Error("accountNotActivated");
-        error.code = 403;
-        throw error;
-      } else if (!isAccountApproved && userType === 'instructor' && isApproveInstructorAccounts) {
-        errorMessage = 'accountNotActivated'
-        const error = new Error("accountNotActivated");
-        error.code = 403;
-        throw error;
+    }
+
+
+    if (decodedToken) {
+      if (!decodedToken) {
+        req.studentIsAuth = false;
+        req.instructorIsAuth = false;
       }
 
-      else {
+      if (decodedToken.studentId) {
+        userType = 'student'
+      }
+      if (decodedToken.instructorId) {
+        userType = 'instructor'
+      }
+      try {
         if (userType === 'instructor') {
-          req.instructorIsAuth = true
+          id = decodedToken.instructorId
         }
         if (userType === 'student') {
-          req.studentIsAuth = true
+          id = decodedToken.studentId
         }
-        req.userId = id
-        // console.log('authenticated!')
-      }
-    } catch (e) {
-      console.log('in catch block', e)
-      const error = {
-        errors: [{ message: errorMessage }],
-        data: {
-          errors: [{ message: errorMessage }]
+        const user = await require(`../models/${userType}`).findById(id)
+        if (!user) {
+          next()
         }
-      }
-      return res.status(403).send(error);
-    }
-    console.log('next')
-    next();
+        const isAccountSuspended = user.isAccountSuspended
+        const isAccountApproved = user.isAccountApproved
 
-  } else {
+        const admin = await Instructor.findOne({ admin: true }).populate(
+          "configuration"
+        );
+        const adminSettings = admin._doc.configuration;
+        const isApproveStudentAccounts = adminSettings.isApproveStudentAccounts;
+        const isApproveInstructorAccounts = adminSettings.isApproveInstructorAccounts;
+        if (isAccountSuspended) {
+          errorMessage = 'accountSuspended'
+          const error = new Error("accountSuspended");
+          error.code = 403;
+          throw error;
+        } else if (!isAccountApproved && userType === 'student' && isApproveStudentAccounts) {
+          errorMessage = 'accountNotActivated'
+          const error = new Error("accountNotActivated");
+          error.code = 403;
+          throw error;
+        } else if (!isAccountApproved && userType === 'instructor' && isApproveInstructorAccounts) {
+          errorMessage = 'accountNotActivated'
+          const error = new Error("accountNotActivated");
+          error.code = 403;
+          throw error;
+        }
 
-    let ticket;
-    console.log('process.env.GOOGLE_CLIENT_ID',process.env.GOOGLE_CLIENT_ID);
-    console.log('token',token)
-    try {
-      console.log('trying to get ticket')
-      ticket = await client.verifyIdToken({
-        idToken: token,
-        audience: process.env.GOOGLE_CLIENT_ID,
-      });
-      console.log('ticket...: ',ticket)
-    } catch (e) {
-      console.log('failed to get google ticket')
-    }
-    if (ticket) {
-      try {
-        const payload = ticket.getPayload()
-        req.googleUser = payload
-        req.isGoogleAuth = true
-        console.log('payload: ',payload)
-        console.log('next')
-        next();
-        
+        else {
+          if (userType === 'instructor') {
+            req.instructorIsAuth = true
+          }
+          if (userType === 'student') {
+            req.studentIsAuth = true
+          }
+          req.userId = id
+          // console.log('authenticated!')
+        }
       } catch (e) {
-        console.log('e', e)
+        console.log('in catch block', e)
+        const error = {
+          errors: [{ message: errorMessage }],
+          data: {
+            errors: [{ message: errorMessage }]
+          }
+        }
+        return res.status(403).send(error);
       }
-    }
-    // else {
-    //   return next()
-    // }
-    console.log('isticket',!!ticket)
-    console.log('no token nor ticket')
-    // console.log('next')
-    // next();
-    
+      console.log('next')
+      next();
 
+    } else {
+
+      let ticket;
+      console.log('process.env.GOOGLE_CLIENT_ID', process.env.GOOGLE_CLIENT_ID);
+      console.log('token', token)
+      try {
+        console.log('trying to get ticket')
+        ticket = await client.verifyIdToken({
+          idToken: token,
+          audience: process.env.GOOGLE_CLIENT_ID,
+        });
+        console.log('ticket...: ', ticket)
+      } catch (e) {
+        console.log('failed to get google ticket')
+      }
+      if (ticket) {
+        try {
+          const payload = ticket.getPayload()
+          req.googleUser = payload
+          req.isGoogleAuth = true
+          console.log('payload: ', payload)
+          console.log('next')
+          next();
+
+        } catch (e) {
+          console.log('e', e)
+        }
+      }
+      // else {
+      //   return next()
+      // }
+      console.log('isticket', !!ticket)
+      console.log('no token nor ticket')
+      // console.log('next')
+      // next();
+
+
+    }
   }
 };
